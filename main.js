8bc28978274d0dcff06447a31550f2356a6d0391
valourme <valourme@franklin.asmallorange.com>
2014-06-26 23:42:33 -0400

    before overhaul to google web designer

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-/* helper functions */
+                                /* helper functions */
 
 function random(min,max){
     return Math.floor((Math.random() * max) + min);
 }
 
+
+jQuery.fn.calcbox = function(){
+    var box = {};
+   var offset = $(this).offset();
+   box.x = offset.left;
+   box.y = offset.top;
+   box.w = $(this).width();
+   box.h = $(this).height();
+   box.c = {'x': box.x+(box.w/2) , 'y': box.y+(box.h/2)};
+   return box;
+};
 
 /* global variables */
 var settings = {
@@ -11,10 +22,15 @@ var settings = {
 	screen_height : window.innerHeight,
 	jar_width: 0,
 	jar_height: 0,
-	jar_padding: 0 
+	jar_padding: 0 ,
+	max_width: 3840,
+	max_height:2160
 };
 var jars = [];
 var stars = [];
+/* 0 = jar selectable
+ 1 = star detail */
+var click_mode = 0; 
 
 /* objects */
 function Jar(options){
@@ -34,6 +50,45 @@ Jar.prototype.deselect = function(){
     $('#'+this.id).css('top',(settings.jar_width*0.4)+'px');
     
     //TODO: put away stars
+};
+
+function Star(options){
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+    this.id = options.id || "";
+    this.selected = false;
+}
+
+Star.prototype.select = function(){
+    this.selected = true;
+    $('#star_detail').show();
+    var star = $('#'+this.id).detach();
+    $('#star_detail').prepend(star);
+    $('#'+this.id).animo({animation: "spinner", iterate: "infinite"});
+    $('#stars_layer img').animo('blur', {duration: 1, amount: 20});
+      $('#jars_layer img').animo('blur', {duration: 1, amount: 20});
+
+    var place = $('#'+this.id).calcbox();
+    $('#'+this.id).animate({'top':place.h/2, 'left':place.w/2}, 1000, function(){
+        $('#'+this.id).animo("cleanse");
+    });
+    click_mode = 1;
+    $('#star_detail button.close').click(this.deselect);
+};
+
+Star.prototype.deselect = function(){
+    this.selected = false;
+	$('#'+this.id).animo("cleanse");
+	var star = $('#'+this.id).detach();
+	$('#stars_layer').append(star);
+	$('#star_detail').hide();
+	$('#stars_layer img').animo("cleanse");
+	$('#'+this.id).css('left',this.x+'px');
+	$('#'+this.id).css('top',this.y+'px');
+	$('#jars_layer img').animo("cleanse");
+$('#star_detail button.close').unbind('click');
+click_mode = 0;	
+
 };
 
 /* functions */
@@ -59,7 +114,7 @@ function addJar(obj, jq){
      jars.push(obj);
      
      jq.click(function(evt){
-         if(obj.selected){
+         if(obj.selected || click_mode==1){
              return;
          }
          
@@ -94,16 +149,13 @@ function getSelectedJar(){
     return result;
 }
 
-/* intialize */
-(function(){
-    setSizes();
-    	
+function makeJars(){
     var img_prefix = 'images/';
     if(settings.jar_width>500){
         img_prefix = 'images/half';
     }
-
-	var jarsLen = data.jars.length;
+    
+    var jarsLen = data.jars.length;
     for(var i=0; i<jarsLen; i++){
         var jardata = data.jars[i];
         
@@ -118,11 +170,100 @@ function getSelectedJar(){
              img.css('top',(settings.jar_width*0.4)+'px');
              var jar = new Jar({id:id});
              addJar(jar,img);
-            if(jardata.title==='me'){
+            if(jardata.title==='software'){
                 jar.select();
             }
     }
+}
+
+function addStar(obj, jq){
+     $('#stars_layer').append(jq);
+     stars.push(obj);
+     
+     jq.click(function(evt){
+         console.log(obj.id+' click');
+         if(obj.selected || click_mode==1){
+             return;
+         }
+         var prevStar = getSelectedStar();
+         if(prevStar!=null){
+             prevStar.deselect();
+         }
+         obj.select();
+     });
+}
+
+function getSelectedStar(){
+    var result = null;
+    for(var i=0; i<jars.length; i++){
+        if(stars[i].selected){
+            result = stars[i];
+            break;
+        }
+    }
+    return result;
+}
+
+function makeStars(){
+    var half = false;
+    var img_prefix;
+    if(settings.screen_width<(settings.max_width/2)){
+        half = true;
+        img_prefix = 'images/halfstar-';
+    } else {
+        img_prefix = 'images/star-';
+    }
+    
+    var xFactor = (settings.screen_width/(settings.max_width/2));
+    var yFactor = (settings.screen_height/(settings.max_height/2));
+    
+    for(var i in data.stars){
+        console.log(i);
+        var starsLen = data.stars[i].length;
+        for(var j=0; j<starsLen; j++){
+            var stardata = data.stars[i][j];
+            var url = img_prefix+stardata.title+'.png';
+                var img = $('<img src="'+url+'" />');
+                img.addClass('star');
+                var id = "star_"+stardata.title;
+                img.attr('id',id);
+                
+                img.css('z-index',(100+j));
+                var left = stardata.x;
+                var top = stardata.y;
+                if(half){
+                    left *= 0.5*xFactor;
+                    top *= 0.5*yFactor;
+                }
+                img.css('left',left+'px');
+                img.css('top',top+'px');
+               // img.css('transform','scale('+xFactor+','+yFactor+')');
+                var star = new Star({'id':id, 'x':left, 'y':top});
+                addStar(star,img);
+                img.load(function(){
+                    console.log(this);
+                    var w = $(this).width()*xFactor;
+                    var h = $(this).height()*yFactor;
+                    $(this).css('width', w +'px');
+                    $(this).css('height', h +'px');
+                });
+        } //end of stars
+    } // end of grouping
+    
+}
+
+/* intialize */
+(function(){
+    setSizes();
+    
+    //stars imit
+    makeJars();
+    
+    // stars init
+    makeStars();
 
 })();
 
 window.onresize = setSizes;
+
+                            
